b0ab27bb3b91f6f206183194a9b83baf32e43fa3
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-06 05:41:32 -0800

    intial project

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const icons = document.querySelectorAll('.hero__icons i');
+let numberIcon = 0;
+setInterval(()=>{
+    numberIcon++;
+    let icon = document.querySelector('.hero__icons .change');
+
+    icon.classList.remove('change');
+    if(numberIcon >= icons.length)
+    {
+        icons[0].classList.add('change');
+        numberIcon=0
+    }else{
+        icon.nextElementSibling.classList.add('change')
+    } 
+},2000)
