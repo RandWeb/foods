4f0b7192b88ada092553ca7211e7fdaf9e5ab273
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-06 12:34:11 -0800

    final template foods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 const icons = document.querySelectorAll('.hero__icons i');
 let numberIcon = 0;
-setInterval(()=>{
+setInterval(() => {
     numberIcon++;
     let icon = document.querySelector('.hero__icons .change');
 
     icon.classList.remove('change');
-    if(numberIcon >= icons.length)
-    {
+    if (numberIcon >= icons.length) {
         icons[0].classList.add('change');
-        numberIcon=0
-    }else{
+        numberIcon = 0
+    } else {
         icon.nextElementSibling.classList.add('change')
-    } 
-},2000)
+    }
+}, 2000)
+
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change');
+    });
+});
